1077e097ce0ab887181509336c9c39f2ea21199a
Smolich404 <86857963+Smolich404@users.noreply.github.com>
2023-06-18 04:51:28 +0200

    Add files via upload

--- Opline Software Lite.bat
+++ Opline Software Lite.bat
@@ -3768,46 +3768,95 @@ if %ERRORLEVEL% == 3 goto Others
 
 :UNI
 cls
-echo.
-echo.
-call :ColorText 1B "###############################################################################################"
-echo.
-echo.
-echo                             ____  _____  _      _____ _   _ ______                      
-echo                            / __ \^|  __ \^| ^|    ^|_   _^| \ ^| ^|  ____^|                     
-echo                           ^| ^|  ^| ^| ^|__) ^| ^|      ^| ^| ^|  \^| ^| ^|__                        
-echo                           ^| ^|  ^| ^|  ___/^| ^|      ^| ^| ^|   \ ^|  __^|                       
-echo                           ^| ^|__^| ^| ^|    ^| ^|____ _^| ^|_^| ^|\  ^| ^|____                      
-echo                            \____/^|_^|    ^|______^|_____^|_^| \_^|______^|                     
-echo                   _____  ____  ______ _________          __     _____  ______ 
-echo                  / ____^|/ __ \^|  ____^|__   __\ \        / /\   ^|  __ \^|  ____^|
-echo                 ^| (___ ^| ^|  ^| ^| ^|__     ^| ^|   \ \  /\  / /  \  ^| ^|__) ^| ^|__   
-echo                  \___ \^| ^|  ^| ^|  __^|    ^| ^|    \ \/  \/ / /\ \ ^|  _  /^|  __^|  
-echo                  ____) ^| ^|__^| ^| ^|       ^| ^|     \  /\  / ____ \^| ^| \ \^| ^|____ 
-echo                 ^|_____/ \____/^|_^|       ^|_^|      \/  \/_/    \_\_^|  \_\______^|
-echo.
-echo.
-call :ColorText 0A "                                            L I T E"
-echo.
-echo.
-echo.
-call :ColorText 1B "###############################################################################################"
-echo.
-echo.
-echo           Enter the address of the folder location where Edge is installed i.e. from
-echo            C:\Program Files (x86)\Microsoft\Edge\Application\91.0.864.41\Installer
-echo                               and paste this address 91.0.864.41
-echo.
-echo            Wpisz adres polozenia folderu na ktorym jest zainstalowany Edge czyli z
-echo            C:\Program Files (x86)\Microsoft\Edge\Application\91.0.864.41\Installer
-echo                                i wklejasz ten adres 91.0.864.41
-echo.              
-set /p NAME=
-if %NAME%== X goto Edge
-if %NAME%== x goto Edge
+taskkill /F /IM msedge.exe  >nul 2>&1
+CD %HOMEDRIVE%%HOMEPATH%\Desktop
+echo %CD%
+
+
+REM ************ Main process *****************
+
+echo *** Removing Microsoft Edge ***
+call :killdir C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe
+call :killdir "C:\Program Files (x86)\Microsoft\Edge"
+call :killdir "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+call :killdir "C:\Program Files (x86)\Microsoft\EdgeCore"
+call :killdir "C:\Program Files (x86)\Microsoft\EdgeWebView"
+echo *** Modifying registry ***
+call :editreg
+echo *** Removing shortcuts ***
+call :delshortcut "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+call :delshortcut "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
+call :delshortcut "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"
+Reg del "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge" /f
+Reg del "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update" /f
+Reg del "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft EdgeWebView" /f
+echo Finished!
+goto NEdge
+
+REM ************ KillDir: Take ownership and remove a directory *****************
+
+:killdir
+echo|set /p=Removing dir %1
+if exist %1 (
+	takeown /a /r /d Y /f %1 > NUL
+	icacls %1 /grant administrators:f /t > NUL
+	rd /s /q %1 > NUL
+	if exist %1 (
+		echo ...Failed.
+	) else (
+		echo ...Deleted.
+	)
+) else (
+	echo ...does not exist.
+)
+exit /B 0
+
+REM ************ Edit registry to add do not update Edge key *****************
+
+:editreg
+echo|set /p=Editting registry
+echo Windows Registry Editor Version 5.00 > RemoveEdge.reg
+echo. >> RemoveEdge.reg
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\EdgeUpdate] >> RemoveEdge.reg
+echo "DoNotUpdateToEdgeWithChromium"=dword:00000001 >> RemoveEdge.reg
+echo. >> RemoveEdge.reg
+echo [-HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{9459C573-B17A-45AE-9F64-1857B5D58CEE}] >> RemoveEdge.reg
+
+regedit /s RemoveEdge.reg
+del RemoveEdge.reg
+echo ...done.
+exit /B 0
+
+:: Delete Microsoft Edge Folders
+rmdir /s /q "C:\Program Files (x86)\Microsoft\Edge"
+rmdir /s /q "C:\Program Files (x86)\Microsoft\EdgeCore"
+rmdir /s /q "C:\Program Files (x86)\Microsoft\EdgeWebView"
+rmdir /s /q "C:\Program Files (x86)\Microsoft\Temp"
+
+:: Delete Edge Icons, from all users
+for /f "delims=" %%a in ('dir /b "C:\Users"') do (
+del /S /Q "C:\Users\%%a\Desktop\edge.lnk" >nul 2>&1
+del /S /Q "C:\Users\%%a\Desktop\Microsoft Edge.lnk" >nul 2>&1)
+
+:: Delete additional files
+if exist "C:\Windows\System32\MicrosoftEdgeCP.exe" (
+for /f "delims=" %%a in ('dir /b "C:\Windows\System32\MicrosoftEdge*"') do (
+takeown /f "C:\Windows\System32\%%a" > NUL 2>&1
+icacls "C:\Windows\System32\%%a" /inheritance:e /grant "%UserName%:(OI)(CI)F" /T /C > NUL 2>&1
+del /S /Q "C:\Windows\System32\%%a" > NUL 2>&1))
+
+:delshortcut
+echo|set /p=Removing shortcut %1
+if exist %1 (
+	del %1
+	echo ...done.
+) else (
+	echo ...does not exist.
+)
+exit /B 0
+
+:NEdge
 cls
-cd C:\Program Files (x86)\Microsoft\Edge\Application\%NAME%\Installer
-setup.exe --uninstall --system-level --verbose-logging --force-uninstall
 cd C:\Users\%username%\Downloads
 mkdir Edge
 cd C:\Users\%username%\Downloads\Edge
